7217c40d2fc30d94d526859ca86f50db047ac915
Veronika Bokan <bokan.veronika@gmail.com>
2020-03-02 10:19:31 +0200

    Added model Word.cs

---
+++ Models/Word.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWord.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+      
+        public string Name { get; set; }
+        public string Pronunciation { get; set; }
+        public string Definition { get; set; }
+        public string Translation { get; set; }
+
+        public int Level { get; set; }
+    }
+}
